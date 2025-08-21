97f09c6a70525d74b8a51c3a0798939f90fa570b
Hidsquid97 <ted111712@gmail.com>
2025-08-21 18:52:28 +0900

    Update door_hacking_6.py

--- Course5/Step1/door_hacking_6.py
+++ Course5/Step1/door_hacking_6.py
@@ -1,3 +1,7 @@
+import multiprocessing as mp
+
+import pyzipper
+
 PWD_LEN = 6
 ALPHANUM = 'abcdefghijklmnopqrstuvwxyz0123456789'
 NUMALPHA = '0123456789abcdefghijklmnopqrstuvwxyz'
@@ -14,16 +18,61 @@ def create_password(index: int):
     return ''.join(tmp)
 
 
-def calculate_range(workers: int = 6):
+def calculate_range(filepath, workers: int = 6):
+    ranges = []
     for i in range(workers):
         term = TOTAL_CASE // workers
         start = term * i
         end = term * (i + 1) - 1
+        ranges.append((filepath, start, end))
 
-    return start, end
+    return ranges
 
 
-def main(): ...
+def search_smallest_file(zf):
+    info_list = sorted(zf.infolist(), key=lambda x: x.file_size)
+    return info_list[0].filename
+
+
+def is_unzipped(zf, smallest_file, pwd):
+    try:
+        zf.read(smallest_file, pwd=pwd.encode('utf-8'))
+
+        return True
+    except RuntimeError:
+        return False
+
+
+def unzip_file(file_path, start, end):
+    try:
+        with pyzipper.AESZipFile(file_path) as zf:
+            smallest_file = search_smallest_file(zf)
+            for i in range(start, end + 1):
+                pwd = create_password(i)
+
+                if is_unzipped(zf, smallest_file, pwd):
+                    return pwd
+
+    except Exception as e:
+        print(e)
+
+
+def main():
+    try:
+        # print(calculate_range())
+        file_path = 'Course5/Step1/emergency_storage_key.zip'
+        # file_path = 'Course5/Step1/mars_base.zip'
+        workers = 6
+        ranges = calculate_range(file_path)
+        with mp.Pool(processes=workers) as pool:
+            print('pool start')
+            results = pool.starmap(unzip_file, ranges)
+
+            print(results)
+    except KeyboardInterrupt:
+        print('shit!')
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
