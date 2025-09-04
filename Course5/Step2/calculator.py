e9c5012b7c396f4049e8dc8e358f285f7e84a62c
ted111712 <ted111712@gmail.com>
2025-09-04 18:53:02 +0900

    Refactor(5-2-2): 메소드 이름 변경 및 순서 재배치
    - 메소드 이름 변경
    - 메소드 순서를 논리적 흐름에 맞게 조정

--- Course5/Step2/calculator.py
+++ Course5/Step2/calculator.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import QApplication, QGridLayout, QLineEdit, QPushButton, Q
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self._init_ui()
 
         self.waiting_operand = []
         self.waiting_operator = None
@@ -17,7 +17,7 @@ class Calculator(QWidget):
 
         self.reset()
 
-    def initUI(self):
+    def _init_ui(self):
         self.setWindowTitle('계산기')
         self.setGeometry(300, 300, 300, 400)
 
@@ -46,88 +46,93 @@ class Calculator(QWidget):
 
             btn = QPushButton(button)
             btn.setStyleSheet('font-size: 20px; height: 50px;')
-            btn.clicked.connect(self.button_clicked)
+            btn.clicked.connect(self._button_clicked)
 
             grid.addWidget(btn, position[0], position[1])
 
-    @staticmethod
-    def format_number(number: float) -> str:
-        return str(number).rstrip('0').rstrip('.')
-
-    def set_text(self, number: float):
-        print(type(number))
-        if number == int(number):
-            number = int(number)
-        text = self.display.text()
-        if text == '0':
-            self.display.setText(str(number))
-        else:
-            self.display.setText(self.display.text() + str(number))
-
-    def button_clicked(self):
+    def _button_clicked(self):
         button = self.sender()
         button_text = button.text()  # type: ignore
 
         match button_text:
             case num if num.isdigit():
-                self.set_text(num)
-                self.waiting_operand.append(num)
-                if self.waiting_operator is not None:
-                    self.operators.append(self.waiting_operator)
-                    self.waiting_operator = None
+                self._handle_number_input(num)
                 print(self.operands)
             case 'AC':
                 self.reset()
             case '%':
                 ...
             case '=':
-                if self.waiting_operand:
-                    self.operands.append(float(''.join(self.waiting_operand)))
-                    self.waiting_operand.clear()
-                self.equal()
+                self._handle_equal_input()
             case '.':
-                ...
+                self._handle_float_input()
             case '±':
                 ...
             case '+' | '−' | '×' | '÷':
-                if self.waiting_operand:
-                    operand = float(''.join(self.waiting_operand))
-                    self.operands.append(operand)
-                self.waiting_operand.clear()
-                self.waiting_operator = button_text
-                self.display.setText(self.display.text() + button_text)
+                self._handle_operator_input(button_text)
 
-    @staticmethod
-    def add(a: float, b: float) -> float:
-        return a + b
+    def _handle_number_input(self, number: str):
+        self._set_text(float(number))
+        self.waiting_operand.append(number)
+        if self.waiting_operator is not None:
+            self.operators.append(self.waiting_operator)
+            self.waiting_operator = None
 
-    @staticmethod
-    def subtract(a: float, b: float) -> float:
-        return a - b
+    def _handle_float_input(self):
+        # 앞에 연산자 혹은 아무런 숫자 입력이 없다면 '0.' 추가
+        # 숫자가 있으면 그냥 . 추가
+        current_text = self.display.text()
+        if current_text[-1] == '.':
+            return
+        if self.waiting_operand:
+            self.display.setText(current_text + '.')
+        else:
+            self.display.setText(current_text + '0.')
+        self.waiting_operand.append('.')
 
-    @staticmethod
-    def multiply(a: float, b: float) -> float:
-        return a * b
+    def _handle_operator_input(self, operator: str):
+        if self.waiting_operand:
+            operand = float(''.join(self.waiting_operand))
+            self.operands.append(operand)
+        self.waiting_operand.clear()
+        self.waiting_operator = operator
+        self.display.setText(self.display.text() + operator)
 
-    @staticmethod
-    def divide(a: float, b: float) -> float | None:
-        if b == 0:
-            raise ZeroDivisionError
-        return a / b
+    def _handle_equal_input(self):
+        if self.waiting_operand:
+            self.operands.append(float(''.join(self.waiting_operand)))
+            self.waiting_operand.clear()
+        result = self._calculate()
 
-    def reset(self):
+        if result is not None:
+            self.display.setText(Calculator._format_number(result))
+
+    def _negative_positive(self): ...
+
+    def _percent(self):
+        # 1. 아무런 입력이 없었을 땐, 그냥 퍼센티지 계산값만 보여주면 됨
+        if not self.waiting_operator:
+            operand = float(''.join(self.waiting_operand))
+            result = operand / 100
+            self.display.setText(str(result))
+            return result
+        # 2. 앞에 연산식이 있으면 해당 결과값에 퍼센티지 계산
+        else:
+            operand = float(''.join(self.waiting_operand))
+            ...
+
+        # ex) 200 + 30% + 100 + 30% = 468
+        # 3. 모듈러 연산
+
+    def reset(self, update_display: bool = True):
         self.operators.clear()
         self.operands.clear()
         self.waiting_operand.clear()
         self.waiting_operator = None
-        self.display.setText('0')
+        if update_display:
+            self.display.setText('0')
 
-    def negative_positive(self): ...
-    def percent(self, number: float):
-        result = number / 100
-        self.display.setText(str(result))
-
-    def equal(self):
+    def _calculate(self) -> float | None:
         try:
             if not self.operands:
                 return
@@ -138,9 +143,12 @@ class Calculator(QWidget):
                     a, b = self.operands[i], self.operands[i + 1]
 
                     if op == '×':
-                        result = Calculator.multiply(a, b)
+                        result = Calculator._multiply(a, b)
                     else:
-                        result = Calculator.divide(a, b)
+                        result = Calculator._divide(a, b)
+
+                        if result is None:
+                            raise ZeroDivisionError
 
                     self.operands[i] = result
 
@@ -155,17 +163,52 @@ class Calculator(QWidget):
                 a = self.operands.pop(0)
                 b = self.operands.pop(0)
                 result = (
-                    Calculator.add(a, b) if op == '+' else Calculator.subtract(a, b)
+                    Calculator._add(a, b) if op == '+' else Calculator._subtract(a, b)
                 )
                 self.operands.insert(0, result)
 
             final_result = self.operands[0]
-            self.display.setText(self.format_number(final_result))
+            return final_result
+
+        except ZeroDivisionError:
+            self.display.setText('정의되지 않음')
+            self.reset(update_display=False)
         except Exception as e:
             print(e)
             self.display.setText('Error!')
+            self.reset(update_display=False)
 
-    def calculate(self): ...
+    def _set_text(self, number: float):
+        print(type(number))
+        if number == int(number):
+            number = int(number)
+        text = self.display.text()
+        if text == '0':
+            self.display.setText(str(number))
+        else:
+            self.display.setText(self.display.text() + str(number))
+
+    @staticmethod
+    def _add(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def _subtract(a: float, b: float) -> float:
+        return a - b
+
+    @staticmethod
+    def _multiply(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def _divide(a: float, b: float) -> float | None:
+        if b == 0:
+            raise ZeroDivisionError
+        return a / b
+
+    @staticmethod
+    def _format_number(number: float) -> str:
+        return str(number).rstrip('0').rstrip('.')
 
 
 if __name__ == '__main__':
